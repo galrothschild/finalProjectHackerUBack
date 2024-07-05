ba23475414972f801322ec695a21015436cb8cfe
rothschild <gal.rothschildf+git@gmail.com>
2024-07-05 20:19:00 +0300

    fixing some minor issues with swagger

--- src/movies/moviesRestController.ts
+++ src/movies/moviesRestController.ts
@@ -52,6 +52,7 @@ router.get("/", async (req, res, next) => {
  *      - name: id
  *        in: path
  *        description: id
+ *        required: true
  *        schema:
  *          type: number
  *     responses:
