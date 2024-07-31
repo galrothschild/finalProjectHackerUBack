991058e69a534f1a06c36b80a6ba4a5c67cd80dd
rothschild <gal.rothschildf+git@gmail.com>
2024-07-31 19:04:04 +0300

    making swagger work with json instead of jsdoc

--- src/swagger.config.ts
+++ src/swagger.config.ts
@@ -1,32 +1,20 @@
-import swaggerJSDoc from "swagger-jsdoc";
 import type { Express, Request, Response } from "express";
 import swaggerUi from "swagger-ui-express";
-export const options: swaggerJSDoc.Options = {
-	definition: {
-		openapi: "3.0.0",
-		info: {
-			title: "Student Management System",
-			version: "1.0.0",
-			description:
-				"Student Management System covered Create, Read, Update, and Delete operations using a Node.js API",
-		},
-		servers: [{ url: "http://localhost:3000/" }],
-	},
+import fs from "node:fs";
+import logger from "./utils/logger/logger.js";
 
-	apis: [
-		"./src/movies/routes/moviesRestController.ts",
-		"./src/tv/routes/TvRestController.ts",
-		"./src/users/routes/usersRestController.ts",
-	],
-};
-const swaggerSpec = swaggerJSDoc(options);
+const PORT = process.env.PORT || 3000;
+
+const swaggerDocument = JSON.parse(fs.readFileSync("src/swagger.json", "utf8"));
+
+swaggerDocument.servers[0].url = `http://localhost:${PORT}`;
 
 export const swaggerDocs = (app: Express, port: number) => {
-	app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+	app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 	app.get("docs.json", (_req: Request, res: Response) => {
 		res.setHeader("Content-Type", "application/json");
-		res.send(swaggerSpec);
+		res.send(swaggerDocument);
 	});
-	console.log(`Docs available at http://localhost:${port}/api-docs`);
+	logger.info(`Docs available at http://localhost:${port}/api-docs`);
 };
